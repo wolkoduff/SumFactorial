d3336f02bb078e584782f0dbc91e923f4ce1ae17
wolkoduff <55200813+wolkoduff@users.noreply.github.com>
2019-10-19 21:47:00 +0300

    Update sumFact.py
    Добавлен код с циклом for, оптимизирован код

--- sumFact.py
+++ sumFact.py
@@ -1,12 +1,25 @@
+# версия через цикл while
 n = int(input())
-summa = 0
-factorial = 1
-i = 1
+ summa = 0
+ factorial = 1
+ i = 1
 
-while n >= 1:
+ while n >= 1:
     factorial *= i
     summa += i/factorial
     i += 1
     n -= 1
 
-print(summa)
+ print(summa)
+ 
+# Версия без цикла while
+
+n = int(input())
+b = 0
+factorial = 1
+
+for i in range(1, n, 1):
+    factorial *= i
+    b += i / factorial
+
+print(b)
